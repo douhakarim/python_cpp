71a71ab164b6482ec762ee25439469f4a5424db5
douhakarim <douhakarim27@gmail.com>
2021-03-08 15:04:10 +0000

    hello/hello_component.cpp

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -1,22 +1,26 @@
 #include <pybind11/pybind11.h>
 
-char version[]="1.0";
+struct Country {
+Country (const std::string &name) : name(name) { }
+void setName(const std::string &name_) { name = name_; }
+const std::string &getName() const { return name; }
+void setCapital(const std::string &capital_) { capital = capital_; }
+const std::string &getCapital() const { return capital; }
 
-char const* getVersion() {
-	return version;
-}
 
-char const* greet() {
-  return "hello, world";
-}
+std::string name;
+std::string capital;
+};
 
 namespace py = pybind11;
 
 
-PYBIND11_MODULE(hello_component,greetings)
-{
-  greetings.doc() = "greeting_object 1.0";
-  
-  greetings.def("greet", &greet, "a function saying hello");
-  greetings.def("getVersion", &getVersion, "a function returning the version");
+PYBIND11_MODULE(hello_component, m) {
+py::class_<Country>(m, "Country")
+.def(py::init<const std::string &>())
+.def("setName", &Country::setName)
+.def("getName", &Country::getName)
+.def("setCapital",&Country::setCapital)
+.def("getCapital",&Country::getCapital);
 }
+
